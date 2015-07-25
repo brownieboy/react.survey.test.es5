64cf9c37dace2b80714ed75de5c72680bd919c17
Michael Brown <brownieboy@gmail.com>
2015-07-25 22:17:02 +1000

    First commit.  All builds working, including CSS via extract-text-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+var webpack = require("webpack");
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var merge = require('webpack-merge');
+
+var TARGET = process.env.TARGET;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+    entry: [path.resolve(ROOT_PATH, 'app/App')],
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
+    output: {
+        path: path.resolve(ROOT_PATH, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            // test for both js and jsx
+            test: /\.jsx?$/,
+
+            // use babel loader with Stage 1 features
+            loader: 'babel?stage=1',
+
+            // operate only on our app directory
+            include: path.resolve(ROOT_PATH, 'app')
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("css-loader")
+
+            // loaders: ['style', 'css']
+        }]
+    },
+    plugins: [
+        new HtmlwebpackPlugin({
+            title: 'Survey test app, innit',
+            template: 'app/index.html',
+            inject: "body"
+        }),
+        new ExtractTextPlugin("./css/app.css", {
+            allChunks: true
+        })
+    ]
+};
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {
+        module: {
+            loaders: [{
+                test: /\.jsx?$/,
+                loader: 'babel?stage=1',
+                include: path.resolve(ROOT_PATH, 'app')
+            }]
+        }
+    });
+}
+
+if (TARGET === 'dev') {
+    module.exports = merge(common, {
+        module: {
+            loaders: [{
+                test: /\.jsx?$/,
+                loaders: ['react-hot', 'babel?stage=1'],
+                include: path.resolve(ROOT_PATH, 'app')
+            }]
+        }
+    });
+}
